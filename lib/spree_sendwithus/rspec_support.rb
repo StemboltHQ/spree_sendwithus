370175b10cfd0d8547e196e4116e139e7dbc92e3
Richard Wilson <richard@freerunningtech.com>
2015-02-06 11:06:58 -0800

    We use send_email now for rspec_support

--- lib/spree_sendwithus/rspec_support.rb
+++ lib/spree_sendwithus/rspec_support.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
   config.before(:each) do
-    allow(SendWithUs::Api).to receive(:new).and_return(double(send_with: true))
+    allow(SendWithUs::Api).to receive(:new).and_return(double(send_email: true))
   end
 end
